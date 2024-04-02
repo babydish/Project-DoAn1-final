8da4bb812119888e619f6f17968128994a2d7407
Hoang Tuan Anh <th469989@gmail.com>
2024-04-02 16:15:38 +0700

    projectdoan1 update function

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -5,15 +5,15 @@ const { multipleMongooseToObject, mongooseToObject } = require('../../ultill/mon
 class CoursesController {
     // [POST] /course/:id (update)
     update(req, res, next) {
-        Course.updateOne({ id: req.params._id }, req.body)
-            .then(() => res.redirect('http://localhost:3000/me/stored/courses')) // chuyển hướng về trang đc cấu hình. tính chất : Thêm trường Location vào response header của giao thức HTTP 
+        Course.updateOne({ _id: req.params.id }, req.body) // condition,object . _id: req.params.id : _id: database, req.params.name_route
+            .then(() => res.redirect('/me/stored/courses')) // chuyển hướng về trang đc cấu hình. tính chất : Thêm trường Location vào response header của giao thức HTTP 
 
 
     }
 
     // [GET] /course/:_id/edit
     edit(req, res, next) {
-        Course.findOne({ id: req.params._id })
+        Course.findOne({ _id: req.params.id })
             .then(course => {
                 res.render('course/edit', { course: mongooseToObject(course) })
             })
